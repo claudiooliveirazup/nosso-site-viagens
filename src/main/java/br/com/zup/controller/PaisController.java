9f6bec1f2e89dc77127875b3b98922ff64b5dccf
Kefferson Costa <keffersonsc@gmail.com>
2020-10-20 15:36:44 -0300

    Cadastro de país

--- src/main/java/br/com/zup/controller/PaisController.java
+++ src/main/java/br/com/zup/controller/PaisController.java
@@ -1,18 +1,22 @@
 package br.com.zup.controller;
 
 import br.com.zup.controller.dtos.CadastrarPaisRequest;
+import br.com.zup.entity.Pais;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/paises")
 public class PaisController {
 
     @PostMapping
-    public void cadastrar(@RequestBody @Valid CadastrarPaisRequest request, UriComponentsBuilder builder){
-
+    public String cadastrar(@RequestBody @Valid CadastrarPaisRequest request, UriComponentsBuilder builder){
+        Pais pais = null;
+        return null;
     }
 }
--- src/main/java/br/com/zup/controller/dtos/CadastrarPaisRequest.java
+++ src/main/java/br/com/zup/controller/dtos/CadastrarPaisRequest.java
@@ -1,4 +1,16 @@
 package br.com.zup.controller.dtos;
 
+import javax.validation.constraints.NotBlank;
+
 public class CadastrarPaisRequest {
+    @NotBlank
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
