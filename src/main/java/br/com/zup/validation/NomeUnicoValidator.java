8a2e2ef3a6c95048b75a71698ca90907684d2201
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2020-10-22 16:12:01 -0300

    IsValid de NomeUnicoValidator iniciado

--- src/main/java/br/com/zup/validation/NomeUnicoValidator.java
+++ src/main/java/br/com/zup/validation/NomeUnicoValidator.java
@@ -1,14 +1,23 @@
 package br.com.zup.validation;
 
 import br.com.zup.annotation.NomeUnico;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.util.Assert;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.lang.annotation.Annotation;
+import java.util.List;
 
 public class NomeUnicoValidator implements ConstraintValidator<NomeUnico, String> {
     private String campo;
     private Class<?> klass;
+
+    @PersistenceContext
+    EntityManager manager;
 
     @Override
     public void initialize(NomeUnico nomeUnico) {
@@ -18,6 +27,16 @@ public class NomeUnicoValidator implements ConstraintValidator<NomeUnico, String
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(Strings.isBlank(value)) return true;
+
+        Query query = manager.createQuery(
+                "select 1 from " + klass.getName() + " where " + campo + " = :valor"
+        );
+        query.setParameter("valor", value);
+
+        List<?> resultado = query.getResultList();
+        Assert.isTrue(resultado.size() >= 1, "");
+
         return true;
     }
 
